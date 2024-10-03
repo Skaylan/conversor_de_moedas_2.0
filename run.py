107167fc8afc8278552ec447ea8d794a4c4b792c
Lucas Oliveira <lucas_7tv@hotmail.com>
2024-10-03 08:59:50 -0300

    fix: using the real ip addres to run the server

--- run.py
+++ run.py
@@ -6,4 +6,7 @@ load_dotenv()
 app = app
 
 if __name__ == '__main__':
-    app.run(debug=True, port=os.getenv('PORT'))
+    if os.getenv('FLASK_ENV') == 'production':
+        app.run(debug=False, port=os.getenv('PORT'), host='0.0.0.0')
+    else:
+        app.run(debug=True, port=os.getenv('PORT'))
